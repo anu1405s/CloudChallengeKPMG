c3e19c73e586df21ed3f5d106220f228228a075d
anu1405s <59620802+anu1405s@users.noreply.github.com>
2023-10-24 18:35:45 +0530

    nested.py

--- KPMG/Challenge2&3/nested.py
+++ KPMG/Challenge2&3/nested.py
@@ -1,15 +1,9 @@
-def nestedValue(data, key):
-    if isinstance(data, dict):
-        if key in data:
-            return data[key]
-        for k, v in data.items():
-            result = nestedValue(v, key)
-            if result is not None:
-                return result
-    elif isinstance(data, list):
-        for item in data:
-            result = nestedValue(item, key)
-            if result is not None:
-                return result
-    return None
-
+def get_nested_value(data, key):
+    keys = key.split('/')
+    current = data
+    for k in keys:
+        if k in current:
+            current = current[k]
+        else:
+            return None  # Key not found
+    return current
